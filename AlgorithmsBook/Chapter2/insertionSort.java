76236bc4ae6b8b71be5d10ea80a3694dc4144305
leffler <leffler@iastate.edu>
2017-12-15 18:58:51 -0600

    Add files via upload

--- AlgorithmsBook/Chapter2/insertionSort.java
+++ AlgorithmsBook/Chapter2/insertionSort.java
@@ -4,7 +4,7 @@ public class insertionSort{
     public static void main(String[] args){
         int[] listToSort = {5,2,4,6,1,3,30,12,7,9,8};
 
-//sorts listToSort using merge sort
+//sorts listToSort using insertionSort sort
 //outter for loop using control "j" is inefficient, the length of the list is not an accurate measurement
 //to describe how many times it needs to iterate the inner for loop to sort a list
         for(int j = 0; j < listToSort.length; j = j + 1){
